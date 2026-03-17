333ae3b372e59bdc457967282b34cc3adf38c39f
PARTHIV PRATIM SAIKIA <parthivsaikia985@gmail.com>
2026-03-17 16:51:12 +0530

    feat: add methods for GCM encryption

--- internal/encryption/encryption.go
+++ internal/encryption/encryption.go
@@ -60,3 +60,43 @@ func DecryptAge(masterKey string, encryptedData []byte) ([]byte, error) {
 	}
 	return out.Bytes(), nil
 }
+
+func EncryptGCM(key, name string) ([]byte, error) {
+	block, err := aes.NewCipher([]byte(key))
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, err
+	}
+
+	return gcm.Seal(nonce, nonce, []byte(name), nil), nil
+}
+
+func DecryptGCM(key, ciphertext string) ([]byte, error) {
+	block, err := aes.NewCipher([]byte(key))
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonceSize := gcm.NonceSize()
+	if len(ciphertext) < nonceSize {
+		return nil, fmt.Errorf("ciphertext too short")
+	}
+
+	nonce, ciphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
+	return gcm.Open(nil, []byte(nonce), []byte(ciphertext), nil)
+}
