6ffb5c7912fab823bc7a313442f136ec9c1fa21b
PARTHIV PRATIM SAIKIA <parthivsaikia985@gmail.com>
2026-03-17 16:59:13 +0530

    chore: move Unlock function to vault.go

--- internal/store/vault.go
+++ internal/store/vault.go
@@ -1,14 +1,16 @@
 package store
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"path/filepath"
 
 	"github.com/parthivsaikia/enmasec/internal/encryption"
+	"github.com/parthivsaikia/enmasec/internal/utils"
 )
 
-func CreateVault(vaultLocation, masterPassword string) error {
+func CreateVault(vaultLocation, password string, hash []byte) error {
 	if err := os.MkdirAll(vaultLocation, 0o700); err != nil {
 		return fmt.Errorf("unable to create vault %w", err)
 	}
@@ -18,7 +20,12 @@ func CreateVault(vaultLocation, masterPassword string) error {
 		return fmt.Errorf("unable to create key file %w", err)
 	}
 	defer file.Close()
-	data, err := encryption.Encrypt([]byte(KEY_FILE_TEXT), masterPassword)
+
+	if _, err := file.Write(append(hash, []byte("\n")...)); err != nil {
+		return err
+	}
+
+	data, err := encryption.EncryptAge([]byte(KEY_FILE_TEXT), password)
 	if err != nil {
 		return fmt.Errorf("unable to encrypt key file: %w", err)
 	}
