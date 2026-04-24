e8ff76591b3dedfd7b4738c5f0bb3ee530d98eba
PARTHIV PRATIM SAIKIA <parthivsaikia985@gmail.com>
2026-04-24 23:11:52 +0530

    feat: add service layer

--- internal/store/service.go
+++ internal/store/service.go
@@ -1,13 +1,27 @@
 package store
 
 import (
+	"encoding/json"
+	"fmt"
 	"os"
 
+	"github.com/parthivsaikia/enmasec/internal/encryption"
 	"github.com/parthivsaikia/enmasec/internal/service"
 )
 
-func CreateService(path string) error {
+func CreateService(path, password string) error {
 	uuid := service.GenerateUUID()
+	service.LocationUUIDMap.Put(uuid, path)
+	// encrypt this map using age in the file vault/index.age
+	mapData, err := json.Marshal(service.LocationUUIDMap)
+	if err != nil {
+		return err
+	}
+	encryptedData, err := encryption.EncryptAge(mapData, password)
+	if err != nil {
+		return err
+	}
+	fmt.Print(encryptedData)
 
 	if err := os.Mkdir(path, 0o700); err != nil {
 		return err
