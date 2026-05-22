993d60c246b6b7ebc6d1bd89eb5611d4526b6914
PARTHIV PRATIM SAIKIA <parthivsaikia985@gmail.com>
2026-05-22 23:30:07 +0530

    chore: add delete cmd to vault cmd

--- internal/cli/vault/command.go
+++ internal/cli/vault/command.go
@@ -28,6 +28,7 @@ func NewCommand() *cobra.Command {
 	cmd.AddCommand(newListCommand())
 	cmd.AddCommand(newUpdateCommand())
 	cmd.AddCommand(newRepairCommand())
+	cmd.AddCommand(newDeleteCommand())
 	cmd.PersistentFlags().String("dir", "", "add custom location for vault")
 	return cmd
 }
