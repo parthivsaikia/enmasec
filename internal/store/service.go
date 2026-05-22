cf4ee03aa70115e61ba20647dc8cdc39341ab18e
PARTHIV PRATIM SAIKIA <parthivsaikia985@gmail.com>
2026-05-22 21:21:50 +0530

    fix: use WriteFile instead of WriteIndexFile

--- internal/store/service.go
+++ internal/store/service.go
@@ -7,11 +7,12 @@ import (
 
 func CreateService(vaultPath, serviceName string, indexMapData []byte) error {
 	servicePath := filepath.Join(vaultPath, serviceName)
+	indexFilePath := filepath.Join(vaultPath, "index.age")
 	if err := os.Mkdir(servicePath, 0o700); err != nil {
 		return err
 	}
 	// write the given data back to the index file
-	err := WriteIndexFile(vaultPath, indexMapData)
+	err := WriteFile(indexMapData, indexFilePath)
 	if err != nil {
 		os.Remove(servicePath)
 		return err
