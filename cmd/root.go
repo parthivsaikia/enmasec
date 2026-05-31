d727b95fdcbdb0a6129642b16363d144b55451ef
PARTHIV PRATIM SAIKIA <parthivsaikia985@gmail.com>
2026-05-31 07:37:58 +0530

    feat: add the hidden clear-password command to root

--- cmd/root.go
+++ cmd/root.go
@@ -7,6 +7,7 @@ import (
 	"github.com/parthivsaikia/enmasec/internal/cli/account"
 	"github.com/parthivsaikia/enmasec/internal/cli/service"
 	"github.com/parthivsaikia/enmasec/internal/cli/vault"
+	"github.com/parthivsaikia/enmasec/internal/clipboard"
 	"github.com/parthivsaikia/enmasec/internal/config"
 	"github.com/parthivsaikia/enmasec/internal/utils"
 	"github.com/spf13/cobra"
@@ -30,6 +31,7 @@ func init() {
 	rootCmd.AddCommand(vault.NewCommand())
 	rootCmd.AddCommand(service.NewCommand())
 	rootCmd.AddCommand(account.NewCommand())
+	rootCmd.AddCommand(clipboard.ClearPasswordCmd())
 	logger := utils.Logger(os.Stdout)
 	config.Init()
 	if err := config.Load(); err != nil {
