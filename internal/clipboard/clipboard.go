a8739b82476d3c79e8d584829887978ef6fa1a48
PARTHIV PRATIM SAIKIA <parthivsaikia985@gmail.com>
2026-05-31 07:33:56 +0530

    feat: add WriteToClipboard function

--- internal/clipboard/clipboard.go
+++ internal/clipboard/clipboard.go
@@ -1,9 +1,36 @@
+package clipboard
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/atotto/clipboard"
+)
+
 func WriteToClipboard(content string) error {
 	if content == "" {
 		return fmt.Errorf("can't write empty string to clipboard")
 	}
 	if err := clipboard.WriteAll(content); err != nil {
 		return fmt.Errorf("can't write to clipboard: %w", err)
+	}
+	return nil
+}
+
+func ClearClipboard(content string) error {
+	time.Sleep(30 * time.Second)
+	if content == "" {
+		return fmt.Errorf("can't clear empty string from clipboard")
+	}
+	clipboardContent, err := clipboard.ReadAll()
+	if err != nil {
+		return fmt.Errorf("can't read from clipboard: %w", err)
+	}
+	if content == clipboardContent {
+		err = clipboard.WriteAll("")
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }
