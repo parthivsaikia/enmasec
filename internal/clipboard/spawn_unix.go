1a10d58e41122907cef514f8e29bfc987ee25bc2
PARTHIV PRATIM SAIKIA <parthivsaikia985@gmail.com>
2026-05-31 07:37:08 +0530

    feat: add setSysProcAttr function for other OS

---
+++ internal/clipboard/spawn_unix.go
@@ -0,0 +1,14 @@
+//go:build !windows
+
+package clipboard
+
+import (
+	"os/exec"
+	"syscall"
+)
+
+func setSysProcAttr(cmd *exec.Cmd) {
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Setsid: true,
+	}
+}
