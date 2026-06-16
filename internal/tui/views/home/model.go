44987e2d5ea3b1e4891173c2516631ed48119a62
PARTHIV PRATIM SAIKIA <parthivsaikia985@gmail.com>
2026-06-16 18:35:37 +0530

    feat: define the methods of the model struct

--- internal/tui/views/home/model.go
+++ internal/tui/views/home/model.go
@@ -1,3 +1,11 @@
+package home
+
+import (
+	tea "charm.land/bubbletea/v2"
+	"github.com/parthivsaikia/enmasec/internal/models"
+	"github.com/parthivsaikia/enmasec/internal/tui/views/vault"
+)
+
 type currentPane int
 
 const (
@@ -13,4 +21,39 @@ type model struct {
 	vaultModel vault.Model
 	// cursor
 	pane currentPane
+}
+
+func InitialModel() model {
+	return model{
+		vaultModel: vault.New(),
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmds []tea.Cmd
+	var cmd tea.Cmd
+	switch msg := msg.(type) {
+	case tea.KeyPressMsg:
+		switch msg.String() {
+		case "tab":
+			m.pane = (m.pane + 1) % 3
+		}
+	}
+	switch m.pane {
+	case 0:
+		m.vaultModel, cmd = m.vaultModel.Update(msg)
+		cmds = append(cmds, cmd)
+	}
+
+	return m, tea.Batch(cmds...)
+}
+
+func (m model) View() tea.View {
+	v := tea.NewView(m.vaultModel.View())
+	v.AltScreen = true
+	return v
 }
