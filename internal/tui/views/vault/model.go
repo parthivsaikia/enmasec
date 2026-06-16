c63d188abfbc9ff66cd6ad916d69103db6329aea
PARTHIV PRATIM SAIKIA <parthivsaikia985@gmail.com>
2026-06-16 18:38:22 +0530

    feat: add vault pane in tui

---
+++ internal/tui/views/vault/model.go
@@ -0,0 +1,191 @@
+package vault
+
+import (
+	"fmt"
+	"io"
+	"strings"
+
+	"charm.land/bubbles/v2/list"
+	tea "charm.land/bubbletea/v2"
+	"charm.land/lipgloss/v2"
+	"github.com/parthivsaikia/enmasec/internal/config"
+	"github.com/parthivsaikia/enmasec/internal/core"
+	"github.com/parthivsaikia/enmasec/internal/models"
+	"github.com/parthivsaikia/enmasec/internal/tui/components/input"
+	"golang.org/x/term"
+)
+
+type lockedStatus int
+
+const (
+	locked = iota
+	unlocking
+	unlocked
+)
+
+type styles struct {
+	title        lipgloss.Style
+	item         lipgloss.Style
+	selectedItem lipgloss.Style
+	pagination   lipgloss.Style
+	help         lipgloss.Style
+	quitText     lipgloss.Style
+}
+
+func newStyles(darkBG bool) styles {
+	var s styles
+	s.title = lipgloss.NewStyle().MarginLeft(2)
+	s.item = lipgloss.NewStyle().PaddingLeft(4)
+	s.selectedItem = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("170"))
+	s.pagination = list.DefaultStyles(darkBG).PaginationStyle.PaddingLeft(4)
+	s.help = list.DefaultStyles(darkBG).HelpStyle.PaddingLeft(4).PaddingBottom(1)
+	s.quitText = lipgloss.NewStyle().Margin(1, 0, 2, 4)
+	return s
+}
+
+type itemDelegate struct {
+	styles *styles
+}
+
+func (d itemDelegate) Height() int                             { return 1 }
+func (d itemDelegate) Spacing() int                            { return 0 }
+func (d itemDelegate) Update(_ tea.Msg, _ *list.Model) tea.Cmd { return nil }
+func (d itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
+	i, ok := listItem.(entry)
+	if !ok {
+		return
+	}
+
+	str := fmt.Sprintf("%d. %s", index+1, i.vault.Name)
+
+	fn := d.styles.item.Render
+	if index == m.Index() {
+		fn = func(s ...string) string {
+			return d.styles.selectedItem.Render("> " + strings.Join(s, " "))
+		}
+	}
+
+	fmt.Fprint(w, fn(str))
+}
+
+type entry struct {
+	vault  *models.Vault
+	status lockedStatus
+}
+
+func (e entry) FilterValue() string { return e.vault.Name }
+
+type Model struct {
+	vaults          list.Model
+	vaultCreateForm input.VaultCreateForm
+}
+
+func New() Model {
+	vaults := core.GetVaults()
+	var listItems []list.Item
+	var currentVaultIndex int
+
+	for i, vault := range vaults {
+		e := entry{
+			vault:  &vault,
+			status: locked,
+		}
+		listItems = append(listItems, e)
+		if vault.Name == config.Config.CurrentVault {
+			currentVaultIndex = i
+		}
+	}
+
+	s := newStyles(true)
+	delegate := itemDelegate{styles: &s}
+
+	l := list.New(listItems, delegate, 20, 20)
+	l.SetShowStatusBar(false)
+	l.SetShowTitle(false)
+	l.SetShowHelp(false)
+	l.Select(currentVaultIndex)
+	l.DisableQuitKeybindings()
+
+	return Model{
+		vaults:          l,
+		vaultCreateForm: *input.NewVaultCreateForm(),
+	}
+}
+
+func (m Model) Init() tea.Cmd {
+	return nil
+}
+
+func (m Model) Update(msg tea.Msg) (Model, tea.Cmd) {
+	var cmds []tea.Cmd
+
+	if _, ok := msg.(tea.WindowSizeMsg); ok {
+		var cmd tea.Cmd
+
+		m.vaults, cmd = m.vaults.Update(msg)
+		cmds = append(cmds, cmd)
+
+		cmd = m.vaultCreateForm.Update(msg)
+		cmds = append(cmds, cmd)
+
+		return m, tea.Batch(cmds...)
+	}
+
+	if m.vaultCreateForm.IsOpen() {
+		return m.UpdateCreateVaultInput(msg)
+	}
+
+	return m.UpdateList(msg)
+}
+
+func (m Model) UpdateList(msg tea.Msg) (Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyPressMsg:
+		switch msg.String() {
+		case "n":
+			m.vaultCreateForm.Open()
+			return m, m.vaultCreateForm.Init()
+		}
+	}
+	var cmd tea.Cmd
+	m.vaults, cmd = m.vaults.Update(msg)
+	return m, cmd
+}
+
+func (m Model) UpdateCreateVaultInput(msg tea.Msg) (Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyPressMsg:
+		switch msg.String() {
+		case "esc":
+			m.vaultCreateForm.Close()
+			return m, nil
+		}
+	}
+	cmd := m.vaultCreateForm.Update(msg)
+	return m, cmd
+}
+
+func (m Model) View() string {
+	width, height, _ := term.GetSize(0)
+
+	backGroundStr := m.vaults.View()
+
+	if !m.vaultCreateForm.IsOpen() {
+		return backGroundStr
+	}
+
+	overlayStr := m.vaultCreateForm.View()
+
+	alignedStyle := lipgloss.NewStyle().Align(lipgloss.Center)
+	alignedOverlay := alignedStyle.Render(overlayStr)
+
+	layers := []*lipgloss.Layer{
+		lipgloss.NewLayer(backGroundStr),
+		// By using lipgloss.Center for alignment, adding the errors
+		// will naturally expand the overlay downwards while keeping it centered!
+		lipgloss.NewLayer(alignedOverlay).X(width / 2).Y(height / 2).Z(2),
+	}
+
+	compositedView := lipgloss.NewCompositor(layers...)
+	return compositedView.Render()
+}
