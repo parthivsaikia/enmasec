4aa0c54671601ca4f69ecf1dc87264560ca0e592
PARTHIV PRATIM SAIKIA <parthivsaikia985@gmail.com>
2026-06-16 18:43:25 +0530

    chore: change home.Home ot home.InitialModel

--- internal/tui/app.go
+++ internal/tui/app.go
@@ -1,12 +1,18 @@
 package tui
 
 import (
+	"fmt"
+	"os"
+
+	tea "charm.land/bubbletea/v2"
 	"github.com/parthivsaikia/enmasec/internal/tui/views/home"
 )
 
 func App() error {
-	if err := home.Home(); err != nil {
-		return err
+	p := tea.NewProgram(home.InitialModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
 	}
 	return nil
 }
