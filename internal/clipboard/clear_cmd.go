87f55c3ea2d0856f9e7f51fab8122260c60cd8eb
PARTHIV PRATIM SAIKIA <parthivsaikia985@gmail.com>
2026-06-26 20:28:27 +0530

    refactor: pass password to cmd.Stdin instead of calling it one go

--- internal/clipboard/clear_cmd.go
+++ internal/clipboard/clear_cmd.go
@@ -1,6 +1,8 @@
 package clipboard
 
 import (
+	"fmt"
+	"io"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -10,12 +12,27 @@ func ClearPasswordCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:    "clear-password",
 		Hidden: true,
-		Args:   cobra.ExactArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
-			password := args[0]
-			ClearClipboard(password)
+		RunE: func(cmd *cobra.Command, args []string) error {
+			password, err := io.ReadAll(os.Stdin)
+			if err != nil {
+				fmt.Println(err)
+				writeToFile("log.txt", []byte(err.Error()))
+			}
+			fmt.Println(string(password))
+			writeToFile("log.txt", password)
+			if err := ClearClipboard(string(password)); err != nil {
+				writeToFile("log.txt", []byte(err.Error()))
+				return err
+			}
 			os.Exit(0)
+			return nil
 		},
 	}
 	return cmd
+}
+
+func writeToFile(path string, content []byte) {
+	f, _ := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0o644)
+	defer f.Close()
+	f.Write(content)
 }
