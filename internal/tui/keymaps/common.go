696f58538ad243597ccb08a67f01acbf88f606e0
PARTHIV PRATIM SAIKIA <parthivsaikia985@gmail.com>
2026-06-26 20:32:40 +0530

    feat: define common keymap for vim like motions

--- internal/tui/keymaps/common.go
+++ internal/tui/keymaps/common.go
@@ -8,3 +8,12 @@ type KeyMap struct {
 	Up   key.Binding
 	Down key.Binding
 }
+
+var CommonKeyMap = KeyMap{
+	Up: key.NewBinding(
+		key.WithKeys("k", "up"),
+	),
+	Down: key.NewBinding(
+		key.WithKeys("j", "down"),
+	),
+}
